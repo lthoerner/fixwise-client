a5a82bdffd4b41131efadca6bcd274014638522e
lthoerner <lowellashton@gmail.com>
2024-02-28 17:30:57 -0800

    refactor(inventory): make HTML & CSS more idiomatic

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -30,41 +30,46 @@
 	}
 </script>
 
-<div class="header-nav">
-	<a class="header-current" href="/inventory"><h1>Inventory</h1></a>
-	<a href="/tickets"><h1>Tickets</h1></a>
-	<a href="/customers"><h1>Customers</h1></a>
-</div>
-<div class="inventory">
-	<h1>Inventory</h1>
+<nav class="menu">
+	<a href="/inventory">Inventory</a>
+	<a href="/tickets">Tickets</a>
+	<a href="/customers">Customers</a>
+</nav>
+
+<div class="page-body">
 	<div class="table">
 		<div class="row">
-			<h2>SKU</h2>
-			<h2>Name</h2>
-			<h2>Count</h2>
-			<h2>Cost</h2>
-			<h2>Price</h2>
+			<span class="column-name">SKU</span>
+			<span class="column-name">Name</span>
+			<span class="column-name">Count</span>
+			<span class="column-name">Cost</span>
+			<span class="column-name">Price</span>
 		</div>
 		{#each inventory as inventoryItem}
 			<div class="row">
-				<h3 class="grid-item">{formatSku(inventoryItem.sku)}</h3>
-				<h3 class="grid-item">{inventoryItem.display_name}</h3>
-				<h3 class="grid-item">{inventoryItem.count}</h3>
-				<h3 class="grid-item">{formatCurrency(inventoryItem.cost)}</h3>
-				<h3 class="grid-item">{formatCurrency(inventoryItem.price)}</h3>
+				<span class="grid-item">{formatSku(inventoryItem.sku)}</span>
+				<span class="grid-item">{inventoryItem.display_name}</span>
+				<span class="grid-item">{inventoryItem.count}</span>
+				<span class="grid-item">{formatCurrency(inventoryItem.cost)}</span>
+				<span class="grid-item">{formatCurrency(inventoryItem.price)}</span>
 			</div>
 		{/each}
 	</div>
 </div>
 
 <style lang="scss">
-	h1,
-	h2,
-	h3 {
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	p,
+	a,
+	span {
 		font-family: 'Helvetica', sans-serif;
 	}
 
-	.header-nav {
+	.menu {
 		display: flex;
 		flex-direction: row;
 		flex-wrap: nowrap;
@@ -77,7 +82,11 @@
 			padding-bottom: 3px;
 			padding-left: 25px;
 			padding-right: 25px;
+			font-size: 35px;
+			font-weight: bold;
 			text-decoration: none;
+			margin-top: 15px;
+			margin-bottom: 10px;
 			color: black;
 			transition: 0.2s ease-out;
 
@@ -87,44 +96,54 @@
 				background-color: cyan;
 				transition: 0.2s ease-out;
 			}
-
-			&.header-current {
-				color: blue;
-			}
 		}
 	}
 
-	.inventory {
-		.table {
+	.page-body {
+		margin-right: 25px;
+		margin-left: 25px;
+	}
+
+	.table {
+		display: grid;
+		grid-template-columns: repeat(5, 20%);
+		grid-template-rows: auto;
+		margin-top: 25px;
+		padding: 15px;
+		row-gap: 15px;
+		border-radius: 20px;
+		background-color: black;
+
+		span.column-name {
+			font-weight: bold;
+			font-size: 25px;
+			margin-top: 12px;
+			margin-bottom: 12px;
+		}
+
+		span.grid-item {
+			font-size: 20px;
+			margin-top: 12px;
+			margin-bottom: 12px;
+			padding-left: 5px;
+		}
+
+		.row {
 			display: grid;
 			grid-template-columns: repeat(5, 20%);
-			grid-template-rows: auto;
-			padding: 15px;
-			row-gap: 15px;
+			grid-column-start: 1;
+			grid-column-end: 6;
+			padding-left: 15px;
+			padding-right: 15px;
 			border-radius: 20px;
-			background-color: black;
+			background-color: lightgrey;
+			transition: 0.2s ease-out;
 
-			.row {
-				display: grid;
-				grid-template-columns: repeat(5, 20%);
-				grid-column-start: 1;
-				grid-column-end: 6;
-				padding-left: 15px;
-				padding-right: 15px;
-				border-radius: 20px;
-				background-color: lightgrey;
+			&:hover {
+				transform: scale(1.0035) translateY(-2px);
+				border-radius: 19px;
+				background-color: white;
 				transition: 0.2s ease-out;
-
-				&:hover {
-					transform: scale(1.0035) translateY(-2px);
-					border-radius: 19px;
-					background-color: white;
-					transition: 0.2s ease-out;
-				}
-
-				.grid-item {
-					padding-left: 5px;
-				}
 			}
 		}
 	}
