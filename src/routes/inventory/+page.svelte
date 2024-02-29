2291b5481e24068349e0389a31fe43308576657b
lthoerner <lowellashton@gmail.com>
2024-02-28 18:46:34 -0800

    refactor(inventory): add table component rows & column header

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -38,22 +38,24 @@
 
 <div class="page-body">
 	<div class="table">
-		<div class="row">
+		<div class="column-header">
 			<span class="column-name">SKU</span>
 			<span class="column-name">Name</span>
 			<span class="column-name">Count</span>
 			<span class="column-name">Cost</span>
 			<span class="column-name">Price</span>
 		</div>
-		{#each inventory as inventoryItem}
-			<div class="row">
-				<span class="grid-item">{formatSku(inventoryItem.sku)}</span>
-				<span class="grid-item">{inventoryItem.display_name}</span>
-				<span class="grid-item">{inventoryItem.count}</span>
-				<span class="grid-item">{formatCurrency(inventoryItem.cost)}</span>
-				<span class="grid-item">{formatCurrency(inventoryItem.price)}</span>
-			</div>
-		{/each}
+		<div class="table-body">
+			{#each inventory as inventoryItem}
+				<div class="row">
+					<span class="grid-item">{formatSku(inventoryItem.sku)}</span>
+					<span class="grid-item">{inventoryItem.display_name}</span>
+					<span class="grid-item">{inventoryItem.count}</span>
+					<span class="grid-item">{formatCurrency(inventoryItem.cost)}</span>
+					<span class="grid-item">{formatCurrency(inventoryItem.price)}</span>
+				</div>
+			{/each}
+		</div>
 	</div>
 </div>
 
@@ -61,9 +63,9 @@
 	:global(body) {
 		margin: 0;
 		padding: 0;
+		background-color: black;
 	}
 
-	p,
 	a,
 	span {
 		font-family: 'Helvetica', sans-serif;
@@ -106,26 +108,34 @@
 
 	.table {
 		display: grid;
-		grid-template-columns: repeat(5, 20%);
-		grid-template-rows: auto;
 		margin-top: 25px;
 		padding: 15px;
-		row-gap: 15px;
-		border-radius: 20px;
-		background-color: black;
+		border-style: solid;
+		border-width: 2px;
+		border-color: gray;
+		border-radius: 0.6em;
+
+		span {
+			color: white;
+		}
 
 		span.column-name {
+			font-size: 22px;
 			font-weight: bold;
-			font-size: 25px;
-			margin-top: 12px;
-			margin-bottom: 12px;
 		}
 
 		span.grid-item {
-			font-size: 20px;
-			margin-top: 12px;
-			margin-bottom: 12px;
+			font-size: 18px;
 			padding-left: 5px;
+		}
+
+		.table-body {
+			padding-top: 10px;
+			padding-bottom: 10px;
+			border-style: solid;
+			border-width: 2px;
+			border-color: gray;
+			border-radius: 0.6em;
 		}
 
 		.row {
@@ -135,16 +145,25 @@
 			grid-column-end: 6;
 			padding-left: 15px;
 			padding-right: 15px;
-			border-radius: 20px;
-			background-color: lightgrey;
-			transition: 0.2s ease-out;
+			padding-top: 10px;
+			padding-bottom: 10px;
+			transition: 0.3s ease-out;
 
 			&:hover {
 				transform: scale(1.0035) translateY(-2px);
-				border-radius: 19px;
-				background-color: white;
-				transition: 0.2s ease-out;
+				border-radius: 0.65em;
+				transition: 0.3s ease-out;
 			}
+		}
+
+		.column-header {
+			display: grid;
+			grid-template-columns: repeat(5, 20%);
+			grid-template-rows: 1;
+			padding-left: 15px;
+			padding-right: 15px;
+			margin-top: 12px;
+			margin-bottom: 17px;
 		}
 	}
 </style>
