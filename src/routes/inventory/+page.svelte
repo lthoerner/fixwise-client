209c095ae0f57a76f40c048d47f3ba4e4248dfa3
lthoerner <lowellashton@gmail.com>
2024-02-28 21:27:17 -0800

    feat, refactor(inventory): add table menu bar and adjusted hover transition properties on rows

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -22,7 +22,7 @@
 	});
 
 	function formatSku(sku: number) {
-		return '#' + sku.toString().padStart(6, '0');
+		return '#' + sku.toString().padStart(7, '0');
 	}
 
 	function formatCurrency(value: Decimal) {
@@ -37,7 +37,46 @@
 </nav>
 
 <div class="page-body">
-	<div class="table">
+	<div class="table-menu">
+		<div class="quick-search gray-outline">
+			<span>Quick search...</span>
+		</div>
+		<div class="filter gray-outline"><span>Filter</span></div>
+		<div class="spacer"></div>
+		<div class="menu-right">
+			<div class="records-per-page">
+				<div class="label"><span>Records per page:</span></div>
+				<div class="value gray-outline"><span>10</span></div>
+			</div>
+			<div class="page-number">
+				<div class="prefix-label"><span>Page:</span></div>
+				<div class="value gray-outline"><span>1</span></div>
+				<div class="suffix-label"><span>of <strong>15</strong></span></div>
+			</div>
+			<div class="page-navigation">
+				<svg width="14" height="22" viewBox="0 0 14 22" fill="none">
+					<path
+						d="M12 2L3 11L12 20"
+						stroke="white"
+						stroke-opacity="0.5"
+						stroke-width="3"
+						stroke-linecap="round"
+					/>
+				</svg>
+				<svg width="14" height="22" viewBox="0 0 14 22" fill="none">
+					<path
+						d="M2 2L11 11L2 20"
+						stroke="white"
+						stroke-opacity="0.5"
+						stroke-width="3"
+						stroke-linecap="round"
+					/>
+				</svg>
+			</div>
+		</div>
+	</div>
+
+	<div class="table gray-outline">
 		<div class="column-header">
 			<span class="column-name">SKU</span>
 			<span class="column-name">Name</span>
@@ -45,7 +84,7 @@
 			<span class="column-name">Cost</span>
 			<span class="column-name">Price</span>
 		</div>
-		<div class="table-body">
+		<div class="table-body gray-outline">
 			{#each inventory as inventoryItem}
 				<div class="row">
 					<span class="grid-item">{formatSku(inventoryItem.sku)}</span>
@@ -69,6 +108,13 @@
 	a,
 	span {
 		font-family: 'Helvetica', sans-serif;
+	}
+
+	.gray-outline {
+		border-style: solid;
+		border-width: 2px;
+		border-color: gray;
+		border-radius: 0.6em;
 	}
 
 	.menu {
@@ -104,16 +150,53 @@
 	.page-body {
 		margin-right: 25px;
 		margin-left: 25px;
+		padding-top: 25px;
+	}
+
+	.table-menu {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		gap: 15px;
+		font-size: 16px;
+		color: white;
+
+		div {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+		}
+
+		.menu-right {
+			gap: 7px;
+		}
+
+		span {
+			padding-top: 7px;
+			padding-bottom: 7px;
+			padding-left: 10px;
+			padding-right: 10px;
+		}
+
+		.quick-search {
+			flex-grow: 2;
+			max-width: 270px;
+			color: rgba(255, 255, 255, 0.6);
+		}
+
+		.spacer {
+			flex-grow: 1;
+		}
+
+		.page-navigation {
+			gap: 15px;
+		}
 	}
 
 	.table {
 		display: grid;
-		margin-top: 25px;
+		margin-top: 15px;
 		padding: 15px;
-		border-style: solid;
-		border-width: 2px;
-		border-color: gray;
-		border-radius: 0.6em;
 
 		span {
 			color: white;
@@ -132,10 +215,6 @@
 		.table-body {
 			padding-top: 10px;
 			padding-bottom: 10px;
-			border-style: solid;
-			border-width: 2px;
-			border-color: gray;
-			border-radius: 0.6em;
 		}
 
 		.row {
@@ -150,8 +229,10 @@
 			transition: 0.3s ease-out;
 
 			&:hover {
-				transform: scale(1.0035) translateY(-2px);
-				border-radius: 0.65em;
+				transform: translateY(-2px);
+				font-size: 20px;
+				padding-left: 13px;
+				background-color: rgba(255, 255, 255, 0.05);
 				transition: 0.3s ease-out;
 			}
 		}
