605a4e88b5e27d99eac51ac7825ce96666ee88cd
lthoerner <lowellashton@gmail.com>
2024-03-23 22:42:18 -0700

    fix(navbar): minor styling improvements to navbar/header

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -23,20 +23,15 @@
 		background-color: purple;
 
 		a {
-			padding-top: 3px;
-			padding-bottom: 3px;
+			padding-top: 18px;
+			padding-bottom: 13px;
 			padding-left: 25px;
 			padding-right: 25px;
 			text-decoration: none;
-			margin-top: 15px;
-			margin-bottom: 10px;
-			color: black;
-			transition: 0.2s ease-out;
+			transition: 0.23s ease-out;
 
 			&:hover {
-				border-top-left-radius: 17px;
-				border-top-right-radius: 17px;
-				background-color: cyan;
+				background-color: magenta;
 				transition: 0.2s ease-out;
 			}
 		}
