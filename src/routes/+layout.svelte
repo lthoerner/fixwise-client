7724f1dfb09082fa63b1e9cbc4bf7cb7a2bb1d80
lthoerner <lowellashton@gmail.com>
2024-03-23 22:53:57 -0700

    refactor(layout): put navbar and page padding in main layout file for deduplication purposes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,42 @@
 	import '$styles/utility.scss';
 </script>
 
-<slot />
+<nav>
+	<a href="/inventory">Inventory</a>
+	<a href="/tickets">Tickets</a>
+	<a href="/customers">Customers</a>
+</nav>
+
+<div id="page-body">
+	<slot />
+</div>
+
+<style lang="scss">
+	@use '$styles/utility' as utility;
+
+	nav {
+		@include utility.flex-row;
+		@include utility.title-text;
+
+		background-color: purple;
+
+		a {
+			padding-top: 18px;
+			padding-bottom: 13px;
+			padding-left: 25px;
+			padding-right: 25px;
+			text-decoration: none;
+			transition: utility.$transition-standard;
+
+			&:hover {
+				background-color: magenta;
+			}
+		}
+	}
+
+	#page-body {
+		margin-right: 25px;
+		margin-left: 25px;
+		padding-top: 25px;
+	}
+</style>
--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -3,43 +3,4 @@
 	export let data;
 </script>
 
-<nav id="menu">
-	<a href="/inventory">Inventory</a>
-	<a href="/tickets">Tickets</a>
-	<a href="/customers">Customers</a>
-</nav>
-
-<div id="page-body">
-	<Table tableData={data.inventoryJson} tableSchema={data.inventorySchema} />
-</div>
-
-<style lang="scss">
-	@use '$styles/utility' as utility;
-
-	#menu {
-		@include utility.flex-row;
-		@include utility.title-text;
-
-		background-color: purple;
-
-		a {
-			padding-top: 18px;
-			padding-bottom: 13px;
-			padding-left: 25px;
-			padding-right: 25px;
-			text-decoration: none;
-			transition: 0.23s ease-out;
-
-			&:hover {
-				background-color: magenta;
-				transition: 0.2s ease-out;
-			}
-		}
-	}
-
-	#page-body {
-		margin-right: 25px;
-		margin-left: 25px;
-		padding-top: 25px;
-	}
-</style>
+<Table tableData={data.inventoryJson} tableSchema={data.inventorySchema} />
